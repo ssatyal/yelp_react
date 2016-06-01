596cba2a1859ba06f732569e926978056f5df061
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-01 15:18:53 -0400

    adds css regex in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -40,4 +40,18 @@ var config = getConfig({
   clearBeforeBuild: true
 });
 
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l &&
+    l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+}
+
+//existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
 module.exports = config;
