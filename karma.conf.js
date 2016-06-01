605519922dc39c094394eb0dcacddcc5fa28ebf1
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-01 16:50:11 -0400

    modifies karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,19 @@
 // Karma configuration
 // Generated on Wed Jun 01 2016 16:43:44 GMT-0400 (EDT)
+var webpackConfig = require('./webpack.config');
 
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['mocha','chai'],
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    },
     plugins: [
       'karma-mocha',
-      'karma-chai'
+      'karma-chai',
+      'karma-webpack'
     ],
     files: [
     ],
