38f48420b0177e2506d618def13580b381fb69a6
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-05 15:11:09 -0400

    able to render map on page

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import Map, {GoogleApiWrapper} from 'google-maps-react'
 
 export class Container extends React.Component {
   render() {
     return (
       <div>
         Hello from the container
+        <Map
+        google={this.props.google} />
       </div>
     )
   }
 }
 
-export default Container
+export default GoogleApiWrapper({
+  apiKey: process.env.GAPI_KEY
+})(Container)
