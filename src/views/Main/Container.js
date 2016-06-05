6f9d77f0fe2254feacbc3c4599840a959a81c191
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-05 15:19:21 -0400

    adds googleApiHelpers.js file

--- src/views/Main/Container.js
+++ src/views/Main/Container.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Map, {GoogleApiWrapper} from 'google-maps-react'
+import {searchNearby} from 'utils/googleApiHelpers'
 
 export class Container extends React.Component {
+  onReady(mapProps, map) {
+    const {google} = this.props;
+    const opts = {
+      location: map.center,
+      radius: '500',
+      types: ['cafe']
+    }
+    searchNearby(google, map, opts)
+      .then((results, pagination) => {
+        //we get some results from a pagination obj
+      }).catch((status, result) => {
+      //there was an error
+      })
+  }
   render() {
     return (
       <div>
-        Hello from the container
         <Map
+        onReady={this.onReady.bind(this)}
         google={this.props.google} />
       </div>
     )
