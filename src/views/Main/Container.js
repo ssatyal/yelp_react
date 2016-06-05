0ddd81618d2dc718af92363238532d418437789a
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-06-05 14:51:00 -0400

    completes main routes.js file

---
+++ src/views/Main/Container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class Container extends react.Component {
+  render() {
+    return (
+      <div>
+        Hello from the container
+      </div>
+    )
+  }
+}
+
+export default Container
